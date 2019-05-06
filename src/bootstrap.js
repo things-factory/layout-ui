54819e0ba3daa064b3b758e52af8f72f6ca035bf
horwengliang <horwengliang@gmail.com>
2019-05-06 17:44:29 +0800

    addEventListener

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,9 +2,15 @@ import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js'
 
 import { store } from '@things-factory/shell'
 
-import { updateLayout } from '@things-factory/layout-base'
+import { updateLayout, OPEN_SNACKBAR } from '@things-factory/layout-base'
 
 export default function bootstrap() {
   import('../src/layouts')
   installMediaQueryWatcher(`(min-width: 460px)`, matches => store.dispatch(updateLayout(matches)))
+  document.addEventListener('notify', function(e) {
+    store.dispatch({
+      type: OPEN_SNACKBAR,
+      message: e.detail
+    })
+  })
 }
