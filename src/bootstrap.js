05c6773a923d5ee1b5c0d8cdeca95fe37821e997
horwengliang <horwengliang@gmail.com>
2019-05-06 18:04:15 +0800

    Remove event open snackbar

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,10 +7,4 @@ import { updateLayout, OPEN_SNACKBAR } from '@things-factory/layout-base'
 export default function bootstrap() {
   import('../src/layouts')
   installMediaQueryWatcher(`(min-width: 460px)`, matches => store.dispatch(updateLayout(matches)))
-  document.addEventListener('notify', function(e) {
-    store.dispatch({
-      type: OPEN_SNACKBAR,
-      message: e.detail
-    })
-  })
 }
