d2b4d60154f9b0132a26b66803300f2239d8dd92
Hearty Oh <heartyoh@hatiolab.com>
2019-05-15 20:44:06 +0900

    v0.0.32

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -5,6 +5,6 @@ import { store } from '@things-factory/shell'
 import { updateLayout, OPEN_SNACKBAR } from '@things-factory/layout-base'
 
 export default function bootstrap() {
-  import('../src/layouts')
+  import('./layouts')
   installMediaQueryWatcher(`(min-width: 460px)`, matches => store.dispatch(updateLayout(matches)))
 }
