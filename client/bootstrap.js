559804197f910c41c7be72c521104b090135c222
Hearty Oh <heartyoh@hatiolab.com>
2019-07-28 22:39:03 +0900

    v0.1.43

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -2,7 +2,8 @@ import { installMediaQueryWatcher } from 'pwa-helpers/media-query.js'
 
 import { updateLayout } from '@things-factory/layout-base'
 
+import './layouts'
+
 export default function bootstrap() {
-  import('./layouts')
   installMediaQueryWatcher(`(min-width: 460px)`, matches => updateLayout(matches))
 }
